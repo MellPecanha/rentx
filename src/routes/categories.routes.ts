80bd05d46d07436d88235d8398f1bf4dc1306b9e
Mell Peçanha <mellpecanha@gmail.com>
2022-05-30 01:15:49 -0300

    feat: add uuid to id

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import {Router} from 'express';
+import {v4 as uuidV4} from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
     const {name, description} = req.body;
 
-    categories.push({
+    const category = {
         name,
         description,
-    });
+        id: uuidV4()
+    };
+
+    categories.push(category);
 
     return res.status(201).send();
 });
