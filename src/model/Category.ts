3953fdefd2e3e9b37af289b36568ef1f716ff9f4
Mell Peçanha <mellpecanha@gmail.com>
2022-05-30 18:37:35 -0300

    feat: create category repository

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,14 +1,14 @@
 
 import {v4 as uuidV4} from 'uuid';
 
-class Category{
+class Category {
     id?: string;
     name: string;
     description: string;
     created_at: Date;
 
-    constructor(){
-        if (!this.id){
+    constructor() {
+        if (!this.id) {
             this.id = uuidV4();
         }
     }
