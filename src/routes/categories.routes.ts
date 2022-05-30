ca980d79c301dd645668178e4b5086d3abd6b26e
Mell Peçanha <mellpecanha@gmail.com>
2022-05-30 20:16:31 -0300

    refactor: add SRP of SOLID concept

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import {Router} from 'express';
-import {idText} from 'typescript';
 
 import {CategoriesRepository} from '../repositories/CategoriesRepository';
+import {CreateCategoryService} from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository();
@@ -9,14 +9,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (req, res) => {
     const {name, description} = req.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-    if(categoryAlreadyExists) {
-        return res.status(400).json({error: 'Category alredy exists!'});
-    }
-
-    categoriesRepository.create({name, description});
-
+    createCategoryService.execute({name, description});
+    
     return res.status(201).send();
 });
 
