b9d34cbdca9849a28ab8c8e0bd2a640994f5dee8
Mell Peçanha <mellpecanha@gmail.com>
2022-05-31 00:05:46 -0300

    feat: create specification repository

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,20 @@
+import {SpecificationsRepository} from '../repositories/SpecificationsRepository';
+
+interface IRequest {
+    name: string;
+    description: string;
+}
 class CreateSpecificationService {
-    execute() {
-        console.log('todo');
+    constructor(private specificationsRepository: SpecificationsRepository) {}
+
+    execute({name, description}: IRequest): void {
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if(specificationAlreadyExists) {
+            throw new Error(`Specification ${name} already exists!`);
+        }
+
+        this.specificationsRepository.create({name, description});
     }
 }
 
