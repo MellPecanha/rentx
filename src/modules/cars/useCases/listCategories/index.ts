eb95edc15804edae915610863914cc73fef98af3
Mell Peçanha <mellpecanha@gmail.com>
2022-05-31 00:54:32 -0300

    refactor: refactor list categories

---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,9 @@
+import {CategoriesRepository} from '../../repositories/CategoriesRepository';
+import {ListCategoriesUseCase} from './ListCategoriesUseCase';
+import {ListCategoriesController} from './ListCategoriesController';
+
+const categoriesRepository = new CategoriesRepository();
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
+
+export {listCategoriesController};
