acac7c2b053106e1486dc01d6337e51c584edc2e
Mell Peçanha <mellpecanha@gmail.com>
2022-05-31 01:15:48 -0300

    refactor: separate repositories

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
-import {Specification} from '../model/Specification';
-import {ICreateSpecificationDTO, ISpecificationRepository} from './ISpecificationRepository';
+import {Specification} from '../../model/Specification';
+import {ICreateSpecificationDTO, ISpecificationRepository} from '../ISpecificationRepository';
 
 class SpecificationsRepository implements ISpecificationRepository {
     private specifications: Specification[];
