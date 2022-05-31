28f8d9629a27e7e4d938b75a2bbfb6970d45f339
Mell Peçanha <mellpecanha@gmail.com>
2022-05-31 01:03:00 -0300

    refactor: use singleton pattern

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import {CategoriesRepository} from '../../repositories/CategoriesRepository';
 import {ListCategoriesUseCase} from './ListCategoriesUseCase';
 import {ListCategoriesController} from './ListCategoriesController';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
