8dbe11cf32605a10ece16edc2fc5f4cfca816c99
Mell Peçanha <mellpecanha@gmail.com>
2022-05-31 01:29:56 -0300

    refactor: create use cases for specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,13 @@
 import {Router} from 'express';
 import {SpecificationsRepository} from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import {CreateSpecificationService} from '../modules/cars/services/CreateSpecificationService';
+import {createSpecificationController} from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
 const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post('/', (req, res) => {
-    const {name, description} = req.body;
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-    createSpecificationService.execute({name, description});
-
-    return res.status(201).send()
+    return createSpecificationController.handle(req, res);
 });
 
 export {specificationsRoutes};
