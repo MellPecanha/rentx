a66ff7706b0deb483cb70d6b7fcf4f721af7f83f
Mell Peçanha <mellpecanha@gmail.com>
2022-05-30 21:11:55 -0300

    refactor: reorganize files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import {Router} from 'express';
 
-import {CategoriesRepository} from '../repositories/CategoriesRepository';
-import {CreateCategoryService} from '../services/CreateCategoryService';
+import {CategoriesRepository} from '../modules/cars/repositories/CategoriesRepository';
+import {CreateCategoryService} from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository();
@@ -12,7 +12,7 @@ categoriesRoutes.post('/', (req, res) => {
     const createCategoryService = new CreateCategoryService(categoriesRepository);
 
     createCategoryService.execute({name, description});
-    
+
     return res.status(201).send();
 });
 
