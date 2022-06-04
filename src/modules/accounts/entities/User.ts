5cea130b3237aee5eff11422bb6e8cc5a8d7deed
Mell Peçanha <mellpecanha@gmail.com>
2022-06-04 18:40:28 -0300

    feat: create user entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,32 @@
+import {Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm";
+import {v4 as uuidV4} from 'uuid';
+
+@Entity('users')
+export class User {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    driver_license: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuidV4();
+        }
+    }
+}
