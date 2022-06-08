8e7b0fc7329a1f630966012103ddfb6265863428
Mell Peçanha <mellpecanha@gmail.com>
2022-06-08 01:07:21 -0300

    feat: add avatar upload

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -6,7 +6,7 @@ class UpdateUserAvatarController {
     async handle(req: Request, res: Response): Promise<Response> {
         const {id} = req.user;
 
-        const avatar_file = null;
+        const avatar_file = req.file.filename;
 
         const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
