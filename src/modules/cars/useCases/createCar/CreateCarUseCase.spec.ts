daefe6400e2b6f8f43912530a792b74ec382e74b
Mell Peçanha <mellpecanha@gmail.com>
2022-06-27 19:57:26 -0300

    test: create test to create car

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,24 @@
+import {CarsRepositoryInMemory} from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import {CreateCarUseCase} from './createCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('create car', () => {
+    beforeEach(() => {
+        carsRepositoryInMemory = new CarsRepositoryInMemory;
+        createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+    });
+
+    it('should be able to create a new car', async () => {
+        await createCarUseCase.execute({
+            name: 'car name',
+            description: 'car description',
+            daily_rate: 100,
+            license_plate: 'license_plate',
+            fine_amount: 99,
+            brand: 'brand',
+            category_id: 'category',
+        });
+    });
+});
