a8274db8bbd9fcf0fbcece596a6ecf3579d72334
Mell Peçanha <mellpecanha@gmail.com>
2022-07-19 18:31:13 -0300

    feat: create user seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,14 +3,14 @@ import express, {NextFunction, Request, Response} from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import {router} from './routes';
-import swaggerFile from '../../../swagger.json';
-
-import '../typeorm';
-
 import '@shared/container';
 import {AppError} from '@shared/errors/AppError';
+import createConnection from '../typeorm';
 
+import swaggerFile from '../../../swagger.json';
+import {router} from './routes';
+
+createConnection();
 const app = express();
 
 app.use(express.json());
