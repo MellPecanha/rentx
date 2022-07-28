79830c34acc70879062d58706a0347d9f0d50755
Mell Peçanha <mellpecanha@gmail.com>
2022-07-28 17:28:45 -0300

    refactor: add primary key for migration create users

--- src/shared/infra/typeorm/migrations/1654376242569-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1654376242569-CreateUsers.ts
@@ -1,40 +1,41 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
 
 export class CreateUsers1654376242569 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "users",
+                name: 'users',
                 columns: [
                     {
-                        name: "id",
-                        type: "uuid",
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
                     },
                     {
-                        name: "name",
-                        type: "varchar",
+                        name: 'name',
+                        type: 'varchar',
                     },
                     {
-                        name: "username",
-                        type: "varchar",
+                        name: 'username',
+                        type: 'varchar',
                         isUnique: true,
                     },
                     {
-                        name: "email",
-                        type: "varchar",
+                        name: 'email',
+                        type: 'varchar',
                     },
                     {
-                        name: "password",
-                        type: "varchar",
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
-                        name: "driver_license",
-                        type: "varchar",
+                        name: 'driver_license',
+                        type: 'varchar',
                     },
                     {
-                        name: "isAdmin",
-                        type: "boolean",
+                        name: 'isAdmin',
+                        type: 'boolean',
                         default: false,
                     },
                     {
