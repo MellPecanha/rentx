57881892962f30f81123958cb46278c87a549e5b
Mell Peçanha <mellpecanha@gmail.com>
2022-08-02 23:12:26 -0300

    feat: apply dates with day.js

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,11 +1,14 @@
 import {AppError} from '@shared/errors/AppError';
 import {RentalsRepositoryInMemory} from '@modules/rentals/infra/typeorm/repositories/in-memory/RentalsRepositoryInMemory';
 import {CreateRentalUseCase} from './CreateRentalUseCase';
+import dayjs from 'dayjs';
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
 describe('create rental', () => {
+    const dayAdd24Hours = dayjs().add(1, 'day').toDate();
+
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
         createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
@@ -15,7 +18,7 @@ describe('create rental', () => {
         const rental = await createRentalUseCase.execute({
             user_id: '1234',
             car_id: '5678',
-            expected_return_date: new Date(),
+            expected_return_date: dayAdd24Hours,
         });
 
         expect(rental).toHaveProperty('id');
@@ -28,13 +31,13 @@ describe('create rental', () => {
             await createRentalUseCase.execute({
                 user_id: '1234',
                 car_id: '5678',
-                expected_return_date: new Date(),
+                expected_return_date: dayAdd24Hours,
             });
 
             await createRentalUseCase.execute({
                 user_id: '1234',
                 car_id: '5678',
-                expected_return_date: new Date(),
+                expected_return_date: dayAdd24Hours,
             });
         }).rejects.toBeInstanceOf(AppError);
     });
@@ -44,13 +47,23 @@ describe('create rental', () => {
             await createRentalUseCase.execute({
                 user_id: '1111',
                 car_id: 'test',
-                expected_return_date: new Date(),
+                expected_return_date: dayAdd24Hours,
             });
 
             await createRentalUseCase.execute({
                 user_id: '2222',
                 car_id: 'test',
                 expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to create a new rental with invalid return time', async () => {
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: '1111',
+                car_id: 'test',
+                expected_return_date: dayjs().toDate(),
             });
         }).rejects.toBeInstanceOf(AppError);
     });
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,7 +1,11 @@
-import {AppError} from '@shared/errors/AppError';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 
 import {IRentalsRepository} from '@modules/rentals/repositories/IRentalsRepository';
 import {Rental} from '@modules/rentals/infra/typeorm/entities/Rental';
+import {AppError} from '@shared/errors/AppError';
+
+dayjs.extend(utc);
 
 interface IRequest {
     user_id: string;
@@ -19,6 +23,8 @@ class CreateRentalUseCase {
         car_id,
         expected_return_date,
     }: IRequest): Promise<Rental> {
+        const minimumHours = 24;
+
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
         if(carUnavailable) {
@@ -29,6 +35,16 @@ class CreateRentalUseCase {
 
         if(rentalOpenToUse) {
             throw new AppError('there is a rental in progress for user');
+        }
+
+        const expectedReturnDateFormat = dayjs(expected_return_date).utc().local().format('DD-MM-YYYY');
+
+        const dateNow = dayjs().utc().local().format('DD-MM-YYYY');
+
+        const compare = dayjs(expectedReturnDateFormat).diff(dateNow, 'hours');
+
+        if(compare < minimumHours) {
+            throw new AppError('invalid return time!');
         }
 
         const rental = await this.rentalsRepository.create({
