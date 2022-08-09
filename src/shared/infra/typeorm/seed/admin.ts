ced600ad4f6129b0fc17c57a5cb1cbceeecb805e
Mell Peçanha <mellpecanha@gmail.com>
2022-08-09 11:39:35 -0300

    fix: add await on admin connection

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -9,12 +9,12 @@ async function create() {
     const id = uuidV4();
     const password = await hash('admin', 8);
 
-    connection.query(
+    await connection.query(
         `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
         VALUES ('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXX')`,
     );
 
-    connection.close;
+    await connection.close;
 }
 
 create().then(() => console.log('user admin created!'));
