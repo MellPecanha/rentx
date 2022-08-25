06437c5ee61e235d44b61cb298093f7da73c1877
Mell Peçanha <mellpecanha@gmail.com>
2022-08-25 17:59:39 -0300

    feat: create password recovery use case and controller

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -0,0 +1,40 @@
+import {IUsersRepository} from '@modules/accounts/repositories/IUsersRepository';
+import {IUsersTokensRepository} from '@modules/accounts/repositories/IUsersTokensRepository';
+import {inject, injectable} from 'tsyringe';
+import {v4 as uuidV4} from 'uuid';
+
+import {IDateProvider} from '@shared/container/providers/dateProvider/IDateProvider';
+
+@injectable()
+class SendForgotPasswordMailUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('UsersTokensRepository')
+        private usersTokensRepository: IUsersTokensRepository,
+
+        @inject('DayJsDateProvider')
+        private dateProvider: IDateProvider,
+    ) {}
+
+    async execute(email: string) {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('User does not exist!');
+        }
+
+        const token = uuidV4();
+
+        const expires_date = this.dateProvider.addHours(3);
+
+        await this.usersTokensRepository.create({
+            refresh_token: token,
+            user_id: user.id,
+            expires_date,
+        });
+    }
+}
+
+export {SendForgotPasswordMailUseCase};
