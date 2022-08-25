2798ec5a22117e696e35642136ac361d807c25a1
Mell Peçanha <mellpecanha@gmail.com>
2022-08-25 12:31:53 -0300

    feat: create user tokens use case

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -7,6 +7,13 @@ interface IUsersTokensRepository {
         refresh_token,
         expires_date,
     }: ICreateUserTokensDTO): Promise<UserTokens>;
+
+    findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string,
+    ): Promise<UserTokens>;
+
+    deleteById(id: string): Promise<void>;
 }
 
 export {IUsersTokensRepository};
---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,62 @@
+import auth from '@config/auth';
+import {IUsersTokensRepository} from '@modules/accounts/repositories/IUsersTokensRepository';
+import {sign, verify} from 'jsonwebtoken';
+import {inject} from 'tsyringe';
+
+import {DayJsDateProvider} from '@shared/container/providers/dateProvider/implementations/DayJsDateProvider';
+import {AppError} from '@shared/errors/AppError';
+
+interface IPayload {
+    sub: string;
+    email: string;
+}
+
+class RefreshTokenUseCase {
+    constructor(
+        @inject('UsersTokensRepository')
+        private usersTokensRepository: IUsersTokensRepository,
+
+        @inject('DayJsDateProvider')
+        private dateProvider: DayJsDateProvider,
+    ) {}
+
+    async execute(token: string): Promise<string> {
+        const {sub, email} = verify(
+            token,
+            auth.secret_refresh_token,
+        ) as IPayload;
+
+        const user_id = sub;
+
+        const userToken =
+            await this.usersTokensRepository.findByUserIdAndRefreshToken(
+                user_id,
+                token,
+            );
+
+        if (!userToken) {
+            throw new AppError('Refresh Token does not exists!');
+        }
+
+        await this.usersTokensRepository.deleteById(userToken.id);
+
+        const refresh_token = sign({email}, auth.secret_refresh_token, {
+            subject: email,
+            expiresIn: auth.expires_in_refresh_token,
+        });
+
+        const expires_date = this.dateProvider.addDays(
+            auth.expires_refresh_token_days,
+        );
+
+        await this.usersTokensRepository.create({
+            user_id,
+            refresh_token,
+            expires_date,
+        });
+
+        return refresh_token;
+    }
+}
+
+export {RefreshTokenUseCase};
