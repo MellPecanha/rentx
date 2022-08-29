021776dda94418da1b457f620e3f6fd60a3e7f9d
Mell Peçanha <mellpecanha@gmail.com>
2022-08-29 20:21:41 -0300

    feat: add template engine to send reset password email

--- src/shared/container/providers/mailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/mailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,11 @@
+import fs from 'fs';
+import handlebars from 'handlebars';
 import nodemailer, {Transporter} from 'nodemailer';
+import {injectable} from 'tsyringe';
 
 import {IMailProvider} from '../IMailProvider';
 
+@injectable()
 class EtherealMailProvider implements IMailProvider {
     private client: Transporter;
 
@@ -21,13 +25,23 @@ class EtherealMailProvider implements IMailProvider {
             .catch(err => console.error(err));
     }
 
-    async sendMail(to: string, subject: string, body: string): Promise<void> {
+    async sendMail(
+        to: string,
+        subject: string,
+        variables: any,
+        path: string,
+    ): Promise<void> {
+        const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+        const templateParse = handlebars.compile(templateFileContent);
+
+        const templateHTML = templateParse(variables);
+
         const message = await this.client.sendMail({
             to,
             from: 'Rentx <noreply@rentx.com.br',
             subject,
-            text: body,
-            html: body,
+            html: templateHTML,
         });
 
         console.log('Message sent: %s', message.messageId);
