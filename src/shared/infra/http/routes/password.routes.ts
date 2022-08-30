89fa5231ecec6de28c47dc7c8a5732da3fb732fd
Mell Peçanha <mellpecanha@gmail.com>
2022-08-30 13:56:44 -0300

    feat: add route and controller for password reset

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,3 +1,4 @@
+import {ResetPasswordUserController} from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import {SendForgotPasswordMailController} from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 import {Router} from 'express';
 
@@ -5,6 +6,10 @@ const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
 
+const resetPasswordUserController = new ResetPasswordUserController();
+
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
 
 export {passwordRoutes};
