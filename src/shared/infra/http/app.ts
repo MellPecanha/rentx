b737f3f8657d15f40abce50a3c55835cd877a7ee
Mell Peçanha <mellpecanha@gmail.com>
2022-09-14 14:53:52 -0300

    feat: apply cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import upload from '@config/upload';
+import cors from 'cors';
 import 'dotenv/config';
 import express, {NextFunction, Request, Response} from 'express';
 import 'express-async-errors';
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
